2e5c9bbc5f131b3c4f9b2857796b57cd34bc9663
mvsanghvi <mevsanghvi@gmail.com>
2024-02-07 17:01:01 -0600

    New

---
+++ 08_Feb_2024/1d.py
@@ -0,0 +1,13 @@
+CC= 184
+CT= 65
+TT= 7
+Total= CC+CT+TT
+Fcc= CC/Total
+Fct= CT/Total
+Ftt= TT/Total
+Pc=((2*CC)+CT)/(2*Total)
+Pt= 1-Pc
+Pcc= (Pc)**2
+Ptt= (Pt)**2
+Pct= 2 *(Pc * Pt)
+#Observed frequencies vs expectations
