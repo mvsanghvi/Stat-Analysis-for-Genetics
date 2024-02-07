7a9367d5608078eb9efcdd999eb4c11915d1d49f
mvsanghvi <mevsanghvi@gmail.com>
2024-02-07 16:50:05 -0600

    New

---
+++ 08_Feb_2024/1c.py
@@ -0,0 +1,16 @@
+CC= 184
+CT= 65
+TT= 7
+Total= CC+CT+TT
+Fcc= CC/Total
+Fct= CT/Total
+Ftt= TT/Total
+Pc=((2*CC)+CT)/(2*Total)
+Pt= 1-Pc
+#Estimate genotype frequencies
+Pcc= (Pc)**2
+print("Pcc=", Pcc)
+Ptt= Pt**2
+print("Ptt=", Ptt)
+Pct= 2 *(Pcc * Ptt)
+print ("Pct=", Pct)
